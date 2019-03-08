f72d94fe92aa3a45fe9cf652cece0128ff8b380b
Neronio17 <ilyaneron@gmail.com>
2019-03-08 22:17:44 +0200

    Added more examples

--- markup/dev/js/main.js
+++ markup/dev/js/main.js
@@ -1,6 +1,13 @@
 'use strict';
 
+////
 let platformStructure = {
+
+    init() {
+      this.box();
+      this.text();
+      this.alert();
+    },
 
 	box: function () {
 		this.el = document.querySelector('.box');
@@ -20,10 +27,10 @@ let platformStructure = {
 
 };
 
-platformStructure.box();
-platformStructure.text();
-platformStructure.alert();
+platformStructure.init();
+////
 
+////
 class Person {
 
     constructor(firstName, lastName) {
@@ -53,3 +60,61 @@ class Student extends Person {
 let student = new Student(1, "Bob", "Smith");
 console.log(student.getFullName());
 console.log(student.getStudentInfo());
+////
+
+
+////
+// объект функции 'getFullName'
+function getFullName() {
+    return this.firstName + " " + this.lastName;
+}
+
+let person1 = {
+    firstName: "Bob",
+    lastName: "Smith",
+    // свойство с именем 'getFullName' привязано к приведенному выше объекту функции 'getFullName'
+    getFullName: getFullName
+};
+
+let person2 = {
+    firstName: "Tim",
+    lastName: "Johnson",
+    // свойство с именем 'getFullName' привязано к приведенному выше объекту функции 'getFullName'
+    getFullName: getFullName
+};
+
+// вывод "Bob Smith"
+console.log(person1.getFullName());
+
+// вывод "Tim   Johnson"
+console.log(person2.getFullName());
+
+// вывод "true" потому что оба свойства указывают на ту же функцию
+console.log(person1.getFullName === person2.getFullName);
+////
+
+
+////
+class test {
+    constructor() {}
+
+    init() {
+        this.alert();
+        prompt(this.text);
+    }
+
+    alert() {
+        this.text = 'text';
+        alert('text');
+        console.log(this.text);
+        document.querySelector('.box').addEventListener('click', new test().alert);
+    }
+}
+
+class test2 extends test {
+    init2() {
+        this.init();
+    }
+}
+
+new test2().init2();
