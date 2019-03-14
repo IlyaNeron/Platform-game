85bc0aef04879886306f92caa8cef4cbc47e45f9
Neronio17 <ilyaneron@gmail.com>
2019-03-14 22:15:27 +0200

    New options for moving, added functional for fixed game cam

--- markup/dev/js/main.js
+++ markup/dev/js/main.js
@@ -1,26 +1,53 @@
 'use strict';
 
 function GameElements() {
-    this.box = HTMLElement;
-    this.background = HTMLElement;
-    this.move_state_left = Boolean;
-    this.move_state_right = Boolean;
+    this.box = document.querySelector('.box');
+    this.background = Array.from(document.querySelectorAll('.background-layer'));
+    this.platform = document.querySelector('.game-platform');
+    this.platform_fixed = document.querySelector('.platform-fixed');
+    this.platform_offset_x = 100;
+    this.platform_offset_y = 0;
+    this.move_state_left = true;
+    this.move_state_right = true;
 }
 
 GameElements.prototype = {
-
     init: function () {
-        this.box = document.querySelector('.box');
-        this.background = Array.from(document.querySelectorAll('.background-layer'));
-        this.bgwidth = this.background.offsetWidth;
-        this.bg_offset = 100;
-        this.move_state_left = true;
-        this.move_state_right = true;
+        this.coordinates();
         this.events();
+    },
+
+    coordinates: function (bgCoordinateX, platformWidth, platformLeft) {
+        this.background.forEach(function (div) {
+            bgCoordinateX = getComputedStyle(div);
+            let matrix = new WebKitCSSMatrix(bgCoordinateX.webkitTransform);
+            this.bg_offset = matrix.m41;
+        }, this);
+
+        platformWidth = getComputedStyle(this.platform).width;
+        platformLeft = getComputedStyle(this.platform).left;
+        console.log(platformWidth);
+        console.log(platformLeft);
     },
 
     events: function () {
         document.addEventListener('keydown', this.backgroundMove.bind(this));
+        document.addEventListener('keydown', this.circleMoveUp.bind(this));
+    },
+
+    platformMoveRight: function () {
+        this.platform_offset_x += .5;
+        this.platform.style.left = -this.platform_offset_x + '%';
+    },
+
+    platformMoveLeft: function () {
+        this.platform_offset_x -= .5;
+        this.platform.style.left = -this.platform_offset_x + '%';
+    },
+
+    circleMoveUp: function (e) {
+        if (e.key === 'ArrowUp') {
+        }
     },
 
     backgroundMove: function (e) {
@@ -28,14 +55,12 @@ GameElements.prototype = {
 
             if (e.key === 'ArrowRight') {
                 this.move_state_left = true;
-                this.bg_offset += 2;
+                this.bg_offset -= 2;
                 this.background.forEach(function (div) {
-                    div.style.transform = 'translate3d(' + -this.bg_offset + '%' + ',' + '0px' + ',' + '0px' + ')';
+                    div.style.transform = 'translate3d(' + this.bg_offset + 'px' + ',' + '0px' + ',' + '0px' + ')';
                 }, this);
+                this.platformMoveRight();
                 this.disableMoveRight();
-                // if (this.bg_offset === -100) {
-                //     this.bg_offset = 10;
-                // }
             }
 
         }
@@ -44,14 +69,12 @@ GameElements.prototype = {
 
             if (e.key === 'ArrowLeft') {
                 this.move_state_right = true;
-                this.bg_offset -= 2;
+                this.bg_offset += 2;
                 this.background.forEach(function (div) {
-                    div.style.transform = 'translate3d(' + -this.bg_offset + '%' + ',' + '0px' + ',' + '0px' + ')';
+                    div.style.transform = 'translate3d(' + this.bg_offset + 'px' + ',' + '0px' + ',' + '0px' + ')';
                 }, this);
+                this.platformMoveLeft();
                 this.disableMoveLeft();
-                // if (this.bg_offset === 100) {
-                //     this.bg_offset = -10;
-                // }
             }
 
         }
@@ -59,14 +82,14 @@ GameElements.prototype = {
     },
 
     disableMoveLeft: function () {
-        if (this.bg_offset === 0) {
+        if (this.bg_offset === 0 || this.platform_offset_x === 0) {
             console.log(this.bg_offset);
             this.move_state_left = false;
         }
     },
 
     disableMoveRight: function () {
-        if (this.bg_offset === 200) {
+        if (this.bg_offset === 200 || this.platform_offset_x === 200) {
             console.log(this.bg_offset);
             this.move_state_right = false;
         }
